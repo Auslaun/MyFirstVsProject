0329cc040b16b7ac888bed248fb19c4391c07c99
Auslaun <assulan.tulegenov@gmail.com>
2026-01-09 15:30:35 +0500

    init: init project

---
+++ Lessons/Main.py
@@ -0,0 +1,25 @@
+print("hello world")
+
+# name1 = "Assulan"
+# Lastname = "Tulegenov"
+# location = "Kazakhstan" # str
+# age = 31 # int
+# height = 1.85 # float
+# print(f"The name is {name1}") 3 f-strings"
+# print(f"The last name is {Lastname}")
+# print(f"The location is {location}")
+# print(f"The age is {age}")
+# print(f"The height is {height} meters")
+# place = "New York"
+# date = "2024-07-15"
+# temperature = 29.5
+# is_windy = False # True - if positive, False - if negative
+# is_rainy = True
+# is_sunny = False
+# print (name)
+# name_new: str = "Assulan"
+# age_new: int = 31
+
+# some_variable: "some text" 
+# some_variable: @@
+
